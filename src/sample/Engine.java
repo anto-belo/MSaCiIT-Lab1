ca667580a91509d928f9533dc2e3eadae063ab8f
anto-belo <70717945+anto-belo@users.noreply.github.com>
2020-09-24 09:53:59 +0300

    test branch

--- src/sample/Engine.java
+++ src/sample/Engine.java
@@ -23,40 +23,44 @@ public class Engine {
                                                         "super", "synchronized", "volatile", "const", "goto", "enum",
                                                         "assert", "transient", "strictfp"};
 
-    private static final String[] methodSigns = {"private", "protected", "public", "static", "final", "void",
-                                                 "abstract", "native", "strictfp"};
+    private static final String[] methodSigns = {"private", "protected", "public", "static", "final", "abstract",
+                                                 "native", "strictfp"};
 
     public static void analyze() {
         operators = new ArrayList<>();
         operands = new ArrayList<>();
         additions = new ArrayList<>();
 
-
         boolean skip = false;
         coder:
         for (String codeLine : code) {
             codeLine = codeLine.trim();
-            if (codeLine.equals("") || tryParseMethodHeader(codeLine)) continue;
-                for (String member : codeLine.trim().split(" ")) {
-                    if (skip) {
-                        skip = false;
-                        continue;
-                    }
-                    String operator;
+            if (codeLine.equals("")) continue;
 
-                    if (member.equals("import") || member.equals("package")) continue coder;
-                    else if (member.equals("class")) {
-                        skip = true;
-                        continue;
-                    }
-
-                    if (isInside(member, allOperators))
-                        addOperator(member);
-                    else if (!isInside(member, otherReservedWords) && (operator = tryToFindOperator(member)) != null)
-                        addOperator(operator);
-                    else if (!isInside(member, otherReservedWords))
-                        addOperand(member);
+            if (isMethodHeader(codeLine)) {
+                getMethodArgs(codeLine);
+                continue;
+            }
+            for (String member : codeLine.trim().split(" ")) {
+                if (skip) {
+                    skip = false;
+                    continue;
                 }
+
+                if (member.equals("import") || member.equals("package")) continue coder;
+                else if (member.equals("class")) {
+                    skip = true;
+                    continue;
+                }
+
+                String operator;
+                if (isInside(member, allOperators))
+                    addOperator(member);
+                else if (!isInside(member, otherReservedWords) && (operator = tryToFindOperator(member)) != null)
+                    addOperator(operator);
+                else if (!isInside(member, otherReservedWords))
+                    addOperand(member);
+            }
         }
 
 
@@ -67,14 +71,50 @@ public class Engine {
         return null;
     }
 
-    private static boolean tryParseMethodHeader(String codeLine) {
+    private static boolean isMethodHeader(String codeLine) {
+        String[] lineMembers = codeLine.split(" ");
+        if (lineMembers.length < 2 || !codeLine.contains("(") || !codeLine.contains(")")) return false;
+        boolean returnTypeDeclared = false;
+        boolean paramsOpen = false;
+        boolean operandDeclared = false;
+        boolean methodOpened = false;
+        for (String lineMember : lineMembers) {
+            if (!isInside(lineMember, methodSigns)) {
+                if (!returnTypeDeclared)
+                    returnTypeDeclared = true;
+                else if (lineMember.contains("(") && !paramsOpen)
+                    paramsOpen = true;
+                else if (paramsOpen && lineMember.charAt(lineMember.length() - 1) == ',') {
+                    operandDeclared = true;
+                } else if (lineMember.contains(")") && paramsOpen) {
+                    paramsOpen = false;
+                } else if (operandDeclared)
+                    operandDeclared = false;
+                else if (!methodOpened && !paramsOpen && lineMember.equals("{"))
+                    methodOpened = true;
+                else return false;
+            }
+        }
+        return true;
+    }
+
+    private static void getMethodArgs(String codeLine) {
+        String[] args = codeLine.substring(codeLine.indexOf('(') + 1, codeLine.indexOf(')')).split(" ");
+        for (int i = 1; i < args.length; i += 2) {
+            String operand = (args[i].charAt(args[i].length() - 1) == ',') ?
+                    args[i].substring(0, args[i].length() - 1) : args[i];
+            addOperand(operand);
+        }
+        addOperator("{");
+    }
+    /* TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST */
+    /*private static boolean tryParseMethodHeader(String codeLine) {
         String[] lineMembers = codeLine.split(" ");
         if (lineMembers.length < 2 || !codeLine.contains("(") || !codeLine.contains(")")) return false;
         boolean returnTypeDeclared = false;
         boolean paramsOpen = false;
         boolean operandAdded = false;
         for (String lineMember : lineMembers) {
-            if (lineMember.equals("void")) returnTypeDeclared = true;
             if (!isInside(lineMember, methodSigns)) {
                 if (!returnTypeDeclared)
                     returnTypeDeclared = true;
@@ -94,7 +134,7 @@ public class Engine {
             }
         }
         return true;
-    }
+    }*/
 
     private static boolean isInside(String x, String[] group) {
         for (String groupMember : group)
